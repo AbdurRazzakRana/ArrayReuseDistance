5aaf1cbb425cb5fdfd6388004aeb91e4b5b8b4ec
AbdurRazzakRana <razzakrana17@gmail.com>
2024-05-10 12:56:42 -0600

    2d loop

--- test_cases.py
+++ test_cases.py
@@ -1,19 +1,5 @@
 from process_dialation import predict_dialated_rf_n_loop_bound
 
-# only inner loop for k = 2 to 4
-# dict2 = {0: 35, 1: 11, 2: 37, 3: 25, 4: 5, 5: 12, 7: 4, 9: 1, 10: 2, 11: 1, -1: 13}
-# dict3 = {0: 43, 1: 15, 2: 53, 3: 37, 4: 5, 5: 20, 9: 6, 12: 1, 13: 2, 14: 2, 15: 1, -1: 17}
-# dict4 = {0: 51, 1: 19, 2: 69, 3: 49, 4: 5, 5: 28, 11: 8, 15: 1, 16: 2, 17: 2, 18: 2, 19:1, -1: 21}
-
-# Predict reuse profile for k = 5
-# print(dict2)
-# print(dict3)
-# print(dict4)
-# print("Predicting for k = 5")
-# predicted_rf = predict_dialated_rf_n_loop_bound(dict2, dict3, dict4, 4)
-# print(predicted_rf)
-# print("Actual for k = 5")
-
 print("----->CASE 1")
 code = '''
     for(k=0;k<5;k++) {
@@ -24,6 +10,7 @@ code = '''
 print(code)
 # print("Mem Trace:  ['retval', 'alpha', 'k', '[2', 'k', 'alpha', 'k', 'arrayidx1', 'k', 'arrayidx4', 'alpha', 'k', 'arrayidx1', 'k', 'k', ']', 'k']")
 print()
+# base cases input rf collected from dynamic rd calculator
 dict2 = {0:5, 1:9, 2:5, -1:7}
 dict3 = {0:7, 1:13, 2:8, -1:9}
 dict4 = {0:9, 1:17, 2:11, -1:11}
@@ -94,6 +81,7 @@ code = '''
 }'''
 print(code)
 print("Predicted for j = 2, k = 300")
+# base cases input rf collected from dynamic rd calculator
 dict2 = {0:15, 1:17, 2:7, 3:9, 4:2, 6:2, -1:10} # j = 2, k = 2
 dict3 = {0:19, 1:25, 2:9, 3:13, 4:4, 8:3, -1:13} # j = 2, k = 3
 dict4 = {0:23, 1:33, 2:11, 3:17, 4:6, 10:4, -1:16} # j = 2, k = 4
@@ -101,6 +89,34 @@ predicted_rf = predict_dialated_rf_n_loop_bound(dict2, dict3, dict4, 299)
 print(predicted_rf)
 print("Actual for j = 2, k = 300")
 actual_rf = {0:1207, 1:2401, 2:603, 3:1201, 4:598, 602:300, -1:904} # j = 2, k = 300
+print(actual_rf)
+if predicted_rf == actual_rf:
+ print("\n----->TEST CASE PASSED\n")
+else:
+ print("\n----->TEST CASE FAILED\n")
+
+
+
+
+print("\n----->CASE 4\n")
+code = '''
+    for(j=0;j<200;j++){
+        for(k=0;k<300;k++) {
+            A[0][k] = alpha * A[0][k];
+            B[j][k] = alpha;
+        }
+    }
+}'''
+print(code)
+print("Predicted for j = 200, k = 300")
+# base cases input rf collected from dynamic rd calculator
+dict_2_300 = {0:1207, 1:2401, 2:603, 3:1201, 4:598, 602:300, -1:904} # j = 2, k = 300
+dict_3_300 = {0:1810, 1:3602, 2:904, 3:1802, 4:897, 602:600, -1:1204} # j = 3, k = 300
+dict_4_300 = {0:2413, 1:4803, 2:1295, 3:2403, 4:1196, 602:900, -1:1504} # j = 4, k = 300
+predicted_rf = predict_dialated_rf_n_loop_bound(dict2, dict3, dict4, 199)
+print(predicted_rf)
+print("Actual for j = 200, k = 300")
+actual_rf = {0:120601, 1:240199, 2:60201, 3:120199, 4:59800, 602:59700, -1:60304} # j = 2, k = 300
 print(actual_rf)
 if predicted_rf == actual_rf:
  print("\n----->TEST CASE PASSED\n")
